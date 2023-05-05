528a8e87c9d5955924c56986388799a1e2d513b1
Valentina <valentinagalyam@gmail.com>
2023-05-05 09:39:21 +0200

    program.cs

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using csharp_scrabble_challenge.Main;
+
+Console.WriteLine("Hello, gamer! Please, enter your word: ");
+var input = Console.ReadLine();
+Scrabble s = new Scrabble(input);
+int result = s.score();
+Console.WriteLine("Your score is: {0} points",  result);
