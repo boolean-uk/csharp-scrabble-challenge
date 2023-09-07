397f85b45762bb966f8fdea0bde4157406d4523a
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-07 15:30:11 +0200

    made it more a game

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -20,18 +20,31 @@ namespace csharp_scrabble_challenge
                 return;
             }
 
-            //Initializing wordlist and the game
+            //Initializing wordlist 
             var wordList = new WordList(fullPathToWordList);
+            Console.WriteLine("Welcome to the Scrabble, the best game ever!");
+            Console.WriteLine("Enter a word to get its score, or press the spacebar to exit the game.");
 
-            //asking user for input
-            Console.WriteLine("Please enter a word to get the matching score:");
-            string userInputWord = Console.ReadLine();
+            //asking user for input and loop untill space is pressed
+            while (true)
+            {            
+                Console.WriteLine("Please enter a word to get the matching score:");
+                string userInputWord = Console.ReadLine();
+       
+                //checking for spacebar pressed
+                if(string.IsNullOrEmpty(userInputWord) || userInputWord == " ")
+                {
+                    Console.WriteLine("Thank you for playing, Goodbye!");
+                    break; // for exeting the loop
+                }
+                //starting game with the user input word
+                var game = new Scrabble(userInputWord, wordList);
 
-            //starting game with the user input word
-            var game = new Scrabble(userInputWord, wordList);
+                //printing out the score for the word
+                Console.WriteLine($"The score for the word {game.GetWord()} is: {game.score()}");
 
-            //printing out the score for the word
-            Console.WriteLine($"The score for the word {game.GetWord()} is: {game.score()}");
+            }
+
         }
     }
 }
