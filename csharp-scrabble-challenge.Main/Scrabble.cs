2389903b1f6bd39e990eadb8198daebfb083b8b1
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-07 10:01:39 +0200

    core-passed-all-tests

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -10,11 +10,9 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
-        public Scrabble
-        {
-            public string word;
+            private string _word; //setting private so cant be accessed outside the class
              //setting op dictionary first
-            public Dictionary<char, int> _valueOfLetters = new Dictionary<char, int>
+            private Dictionary<char, int> _valueOfLetters = new Dictionary<char, int>
             {
                 {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
                 {'D', 2}, {'G', 2},
@@ -25,10 +23,30 @@ namespace csharp_scrabble_challenge.Main
                 {'Q', 10}, {'Z', 10}
             };
           
+        public Scrabble(string word)
+        {
+            _word = word?.Trim().ToUpper();
+            //word? for if word is null then the rest dont get executed
+            // trim for removing whitspace chars, spaces, tabs, newline..
+            //toUpper for for converting string to uppercase
+        }
 
-        public int score()
+        public int score() //method for returning the score
         {
             //TODO: score calculation code goes here
+            if (string.IsNullOrEmpty(_word)) return 0; //if its empty, send back 0
+
+            int score = 0; //setting initials score to 0
+
+            //calculating score
+            foreach(char character in _word)
+            {
+                if (_valueOfLetters.ContainsKey(character))
+                {
+                    score += _valueOfLetters[character];
+                }
+            }
+            return score; //return score
         }
     }
 }
