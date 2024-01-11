5fb586ebd078d80d3768fb158a5b3a933b752d9a
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-01-11 09:55:59 +0100

    Martin S. Johannessen

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -4,20 +4,67 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private string _word;
+        private Dictionary<char, int> _letterValues = new Dictionary<char, int>
+        {
+            {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
+            {'D', 2}, {'G', 2},
+            {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+            {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4},
+            {'K', 5},
+            {'J', 8}, {'X', 8},
+            {'Q', 10}, {'Z', 10}
+        };
+
         public Scrabble(string word)
         {            
-            //TODO: do something with the word variable
+            _word = word.ToUpper();
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+
+            foreach (char letter in _word)
+            {
+                score += _letterValues.GetValueOrDefault(letter, 0);
+            }
+
+            string patternDoubleScore = @"\{(.*?)\}";
+            string patternTrippleScore = @"\[(.*?)\]";
+
+            Match doubleMatch = Regex.Match(_word, patternDoubleScore);
+            Match trippleMatch = Regex.Match(_word, patternTrippleScore);
+
+            if (doubleMatch.Success)
+            {
+                string doubleScoreString = doubleMatch.Groups[1].Value;
+
+                foreach (char letter in doubleScoreString)
+                {
+                    score += _letterValues.GetValueOrDefault(letter, 0);
+                }
+            }
+
+            if (trippleMatch.Success)
+            {
+                string trippleScoreString = trippleMatch.Groups[1].Value;
+
+                foreach (char letter in trippleScoreString)
+                {
+                    score += _letterValues.GetValueOrDefault(letter, 0)*2;
+                }
+            }
+
+            return score;
         }
     }
 }
