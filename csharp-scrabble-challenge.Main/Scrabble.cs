275cefcdce2feb5cfb61f02cc52f80f3ce147ada
Sebank <sebank@gmail.com>
2024-01-11 09:48:55 +0100

    Sebastian Ankill C#3

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -15,14 +15,14 @@ namespace csharp_scrabble_challenge.Main
         private int multiplier = 1;
 
         // number of letters
-        char[] letters = Enumerable.Range('A', 26).Select(x => (char) x).ToArray();
+/*        char[] letters = Enumerable.Range('A', 26).Select(x => (char) x).ToArray();
         int[] scores = { 
             1, 3, 3, 2, 1,
             4, 2, 4, 1, 8, 
             5, 1, 3, 1, 1, 
             3, 10, 1, 1, 1, 
             1, 4, 4, 8, 4, 10 
-        };
+        };*/
         Dictionary<char, int> letterToScore = new Dictionary<char, int>
         {
             {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
@@ -37,7 +37,7 @@ namespace csharp_scrabble_challenge.Main
         public Scrabble(string word)
         {
             //TODO: do something with the word variable
-            _word = word.ToUpper();
+            _word = word.ToUpper().Trim();
 
             /*
             for ( int i = 0; i < letterToScore.Count; i++ )
