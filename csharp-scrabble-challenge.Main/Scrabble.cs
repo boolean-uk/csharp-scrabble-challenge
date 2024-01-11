b652daf27c48cf5da0a444b2dcdd6f185a652830
Eddy1108 <edvardh@hotmail.no>
2024-01-11 09:27:23 +0100

    Update Scrabble.cs

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,69 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        public Dictionary<char, int> LetterValues { get; set; } = new Dictionary<char, int>()
+        {
+            { 'a', 1 },
+            { 'b', 3 },
+            { 'c', 3 },
+            { 'd', 2 },
+            { 'e', 1 },
+            { 'f', 4 },
+            { 'g', 2 },
+            { 'h', 4 },
+            { 'i', 1 },
+            { 'j', 8 },
+            { 'k', 5 },
+            { 'l', 1 },
+            { 'm', 3 },
+            { 'n', 1 },
+            { 'o', 1 },
+            { 'p', 3 },
+            { 'q', 10 },
+            { 'r', 1 },
+            { 's', 1 },
+            { 't', 1 },
+            { 'u', 1 },
+            { 'v', 4 },
+            { 'w', 4 },
+            { 'x', 8 },
+            { 'y', 4 },
+            { 'z', 10 }
+
+        };
+
+        public string Word { get; set; }
+        public int Multiplier { get; set; } = 1;
+
         public Scrabble(string word)
         {            
             //TODO: do something with the word variable
+
+            Word = word.ToLower();
+
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+
+            int score = 0;
+
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] == '{') Multiplier *= 2;
+                if (Word[i] == '[') Multiplier *= 3;
+
+                if (Word[i] == '}') Multiplier /= 2;
+                if (Word[i] == ']') Multiplier /= 3;
+
+                if (LetterValues.ContainsKey(Word[i]))
+                {
+                    score += LetterValues.GetValueOrDefault(Word[i]) * Multiplier;
+                }
+            }
+
+            return score;
         }
     }
 }
