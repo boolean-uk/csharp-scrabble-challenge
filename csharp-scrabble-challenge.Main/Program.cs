26ef81aa477be87d7f2b3f13974fd4703173ac25
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-01-11 09:19:35 +0100

    Completed both Core and Extention.

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_scrabble_challenge.Main;
+
 Console.WriteLine("Hello, World!");
+string word = "a";
+Scrabble test = new Scrabble(word);
+
+
+Console.WriteLine(test.score());
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,79 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        public string Word { get; set; }
         public Scrabble(string word)
         {            
-            //TODO: do something with the word variable
+            Word = word;
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+            int multiplier = 1;
+
+            foreach (char c in Word) 
+            {
+                switch (Char.ToUpper(c))
+                {
+                    case '{':
+                        multiplier = 2;
+                        break;
+                    case '}':
+                        multiplier = 1;
+                        break;
+                    case '[':
+                        multiplier = 3;
+                        break;
+                    case ']':
+                        multiplier = 1;
+                        break;
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                    case 'L':
+                    case 'N':
+                    case 'R':
+                    case 'S':
+                    case 'T':
+                        score += 1 * multiplier;
+                        break;
+                    case 'D':
+                    case 'G':
+                        score += 2 * multiplier;
+                        break;
+                    case 'B':
+                    case 'C':
+                    case 'M':
+                    case 'P':
+                        score += 3 * multiplier;
+                        break;
+                    case 'F':
+                    case 'H':
+                    case 'V':
+                    case 'W':
+                    case 'Y':
+                        score += 4 * multiplier;
+                        break;
+                    case 'K':
+                        score += 5 * multiplier;
+                        break;
+                    case 'J':
+                    case 'X':
+                        score += 8 * multiplier;
+                        break;
+                    case 'Q':
+                    case 'Z':
+                        score += 10 * multiplier;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return score;
         }
     }
 }
