2689b1409127545a02c8a6e78716b59c95acd17b
Sebank <sebank@gmail.com>
2024-01-11 11:21:25 +0100

    added test to check cumulative product

--- csharp-scrabble-challenge.Test/ExtensionTests.cs
+++ csharp-scrabble-challenge.Test/ExtensionTests.cs
@@ -13,6 +13,8 @@ namespace csharp_scrabble_challenge.Test
     {
 
         [TestCase("{street}", 12)] //extension double word
+        [TestCase("{str{e}et}", 14)] //extension double with double word
+        [TestCase("{str[e]et}", 16)] //extension double with tripple word
         [TestCase("[street]", 18)] //extension triple word
         [TestCase("{quirky}", 44)] //extension double word
         [TestCase("[quirky]", 66)] //extension triple word
