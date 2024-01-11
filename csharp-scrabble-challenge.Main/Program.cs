4c166bffd21c53a3dc9109911eebfc7bb9ea7587
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-01-11 16:57:51 +0100

    dog tests

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using csharp_scrabble_challenge.Main;
 
-Console.WriteLine("Du fikk: "+new Scrabble("f").score());
+Console.WriteLine("Du fikk: "+new Scrabble("test").score());
