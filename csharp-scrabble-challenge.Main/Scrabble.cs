e61b51c94c5640d6703c781f25868fdb160a8e89
Victor <victor.adamson@se.experis.com>
2024-01-11 09:51:12 +0100

    Completed core exercise

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
@@ -9,15 +10,55 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private Dictionary<char, int> _scoreSheet;
+        string _word;
+        int totalScore;
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+            _scoreSheet = new Dictionary<char, int>();
+            char[] point1 = "AEIOULNRST".ToCharArray();
+            char[] point2 = "DG".ToCharArray();
+            char[] point3 = "BCMP".ToCharArray();
+            char[] point4 = "FHVWY".ToCharArray();
+            char[] point5 = "K".ToCharArray();
+            char[] point8 = "JX".ToCharArray();
+            char[] point10 = "QZ".ToCharArray();
+            foreach (char value in point1)
+                _scoreSheet.Add(value, 1);
+            foreach (char value in point2)
+                _scoreSheet.Add(value, 2);
+            foreach (char value in point3)
+                _scoreSheet.Add(value, 3);
+            foreach (char value in point4)
+                _scoreSheet.Add(value, 4);
+            foreach (char value in point5)
+                _scoreSheet.Add(value, 5);
+            foreach (char value in point8)
+                _scoreSheet.Add(value, 8);
+            foreach (char value in point10)
+                _scoreSheet.Add(value, 10);
+            
+            _word = word;
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            char[] chars = _word.ToUpper().ToArray();
+            foreach (char letter in chars)
+            {
+                if (_scoreSheet.ContainsKey(letter))
+                {
+                    totalScore += _scoreSheet[letter];
+                }
+                else
+                {
+                    Console.WriteLine($"{letter} is not an accepted letter!");
+                    return 0;
+                }  
+            }
+            return totalScore;
         }
     }
 }
