e8b17a1405f68f955b40f9490be01f01b24a4a2f
Kristian <kristian.hjelmtveit@no.experis.com>
2024-01-11 11:16:26 +0100

    solution

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,58 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private string _word;
+        private int _mult;
+
+        private static Dictionary<char, int> charToScore = new Dictionary<char, int>(){
+            {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1},
+            {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
+            {'D', 2}, {'G', 2},
+            {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+            {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4},
+            {'K', 5},
+            {'J', 8}, {'X', 8},
+            {'Q', 10}, {'Z', 10},
+    };
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+            _word = word.ToUpper();
+            _mult = 1;
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            //TODO: Remove this line when the code has been written
+            Boolean tripleActive = false;
+            Boolean doubleActive = false;
+
+
+            int points = 0;
+            foreach (char c in _word)
+            {
+                switch (c)
+                {
+                    case '{':
+                        _mult *= 2;
+                        break;
+                    case '}':
+                        _mult /= 2;
+                        break;
+                    case '[':
+                        _mult *= 3;
+                        break;
+                    case ']':
+                        _mult /= 3;
+                        break;
+                    default:
+                        break;
+                }
+                points += _mult * charToScore.GetValueOrDefault(c, 0);
+                
+            }
+            return points;
         }
     }
 }
--- csharp-scrabble-challenge.Test/ExtensionTests.cs
+++ csharp-scrabble-challenge.Test/ExtensionTests.cs
@@ -18,6 +18,9 @@ namespace csharp_scrabble_challenge.Test
         [TestCase("[quirky]", 66)] //extension triple word
         [TestCase("{OXyPHEnBUTaZoNE}", 82)]
         [TestCase("[OXyPHEnBUTaZoNE]", 123)]
+        [TestCase("{str{e}et}", 14)]
+        [TestCase("{str[e]et}", 16)]
+
         public void ExtendedCriteriaTests(string word, int targetScore)
         {
             Assert.AreEqual(this.GetWordScore(word), targetScore);
