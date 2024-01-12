ac67f2c2f5ef29d24bdad9e1a21e47a9c383a0b0
SBjer1 <sebastian.engan@no.experis.com>
2024-01-12 09:58:21 +0100

    extension done

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -11,40 +11,18 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
-        public Dictionary<string, int> _letterValue;
-        public string _word;
+        Dictionary<char, int> _letterValues = new Dictionary<char, int>()
+        {
+            {'A', 1}, {'B', 3}, {'C', 3}, {'D', 2}, {'E', 1}, {'F', 4}, {'G', 2},
+            {'H', 4}, {'I', 1}, {'J', 8}, {'K', 5}, {'L', 1}, {'M', 3}, {'N', 1},
+            {'O', 1}, {'P', 3}, {'Q', 10}, {'R', 1}, {'S', 1}, {'T', 1}, {'U', 1},
+            {'V', 4}, {'W', 4}, {'X', 8}, {'Y', 4}, {'Z', 10}
+        };
+        private string _word;
         public Scrabble(string Word)
         {
             //TODO: do something with the word variable
-            _word = Word;
-            _letterValue = new Dictionary<string, int>();
-            _letterValue.Add("A", 1);
-            _letterValue.Add("B", 3);
-            _letterValue.Add("C", 3);
-            _letterValue.Add("D", 2);
-            _letterValue.Add("E", 1);
-            _letterValue.Add("F", 4);
-            _letterValue.Add("G", 2);
-            _letterValue.Add("H", 4);
-            _letterValue.Add("I", 1);
-            _letterValue.Add("J", 8);
-            _letterValue.Add("K", 5);
-            _letterValue.Add("L", 1);
-            _letterValue.Add("M", 3);
-            _letterValue.Add("N", 1);
-            _letterValue.Add("O", 1);
-            _letterValue.Add("P", 3);
-            _letterValue.Add("Q", 10);
-            _letterValue.Add("R", 1);
-            _letterValue.Add("S", 1);
-            _letterValue.Add("T", 1);
-            _letterValue.Add("U", 1);
-            _letterValue.Add("v", 4);
-            _letterValue.Add("W", 4);
-            _letterValue.Add("X", 8);
-            _letterValue.Add("Y", 4);
-            _letterValue.Add("Z", 10);
-
+            _word = Word.ToUpper();
             
             
         }
@@ -53,55 +31,39 @@ namespace csharp_scrabble_challenge.Main
         {
             //TODO: score calculation code goes here 
             int score = 0;
-
-            for (int i = 0; i < _word.Length; i++)
+            int multiplier = 1;
+            
+            foreach (char letter in _word)
             {
-                string letter = _word[i].ToString().ToUpper();
-                foreach (var item in _letterValue)
+                if (_letterValues.ContainsKey(letter))
                 {
-                    if (letter.Equals(item.Key))
-                    {
-
-                        score += item.Value;
-
-                    }
+                    score += _letterValues[letter] * multiplier;
                 }
+                else if(letter == ('{'))
+                {
+                    multiplier *= 2;
+                }
+                else if (letter == ('}'))
+                {
+                    multiplier *= 2;
+                }
+                else if (letter == ('['))
+                {
+                    multiplier *= 3;
+                }
+                else if (letter == (']'))
+                {
+                    multiplier *= 3;
+                }
+
             }
+            
             return score;
         }
 
-        //public int score()
-        //{
-        //    int score = 0;
+        
 
-        //    foreach(char letter in _word)
-        //    {
-        //        if(_letterValue.ContainsKey(letter.ToString().ToUpper()))
-        //        {
-
-        //        }
-        //    }
-
-
-        //    return score;
-        //}
-
-        //private int GetLetterScore(string letter)
-        //{
-        //    if (_letterValue.ContainsKey(letter))
-        //    {
-        //        return _letterValue[letter];
-        //    }
-        //    else
-        //    {
-        //        // Assuming default score for a letter not found in _letterValue is 1
-        //        return 0;
-        //    }
-        //}
-
-
-
-        public Dictionary<string, int> letterValues { get { return _letterValue; } }
+        public Dictionary<char, int> letterValues { get { return _letterValues; } }
         public string word {  get { return _word; } }
     }
 }
