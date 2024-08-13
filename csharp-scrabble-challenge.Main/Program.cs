86b0e6bd2273e7f214f6ce3de066f08586ab5ed7
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-13 14:30:07 +0200

    15/15 without Double and Triple Letter conditions

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string word = "AEIOULNRST";
+char c = 'E';
+
+if (word.Contains(c))
+{
+    Console.WriteLine("HELLO");
+}
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,74 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private string _word;
+        public Dictionary<string, int> letterValues = new Dictionary<string, int>()
+        {
+            {"AEIOULNRST", 1 },
+            {"DG" , 2 },
+            {"BCMP" , 3 },
+            { "FHVWY" , 4 },
+            { "K" , 5 },
+            { "JX" , 8 },
+            { "QZ" , 10 }
+        };
+
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+            string wordInUpper = word.ToUpper();
+            _word = wordInUpper;
+            
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+
+            if (_word == "")
+            {
+                return score;
+            }
+
+            foreach (char c in _word)
+            {
+                foreach (string word in letterValues.Keys) 
+                {
+                    if (word.Contains(c))
+                    {
+                        score += letterValues[word];
+                    }
+                }
+            }
+
+            if (_word.First() == '{' & _word.Last() == '}') 
+            {
+                score *= 2;
+            } else if (_word.First() == '[' & _word.Last() == ']') 
+            {
+                score *= 3;
+            }
+
+            return score;
         }
+
+        /*
+        public int getValueByChar(char character)
+        {
+            int value = 0;
+
+            foreach (string word in letterValues.Keys)
+            {
+                if (word.Contains(character))
+                {
+                    value = letterValues[word];
+                    return value;
+                }
+            }
+            
+            return value;
+        }
+        */
     }
 }
