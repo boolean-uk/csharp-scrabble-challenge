0168da977b8a34c96d48712d711bcfd4b823b529
George <george.alexander.saav@gmail.com>
2024-08-13 13:21:08 +0200

    Solve with extension

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,77 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        Dictionary<char, int> letterValue = new Dictionary<char, int>()
+        {
+            { 'A', 1},
+            { 'E', 1},
+            { 'I', 1},
+            { 'O', 1},
+            { 'U', 1},
+            { 'L', 1},
+            { 'N', 1},
+            { 'R', 1},
+            { 'S', 1},
+            { 'T', 1},
+            { 'D', 2},
+            { 'G', 2},
+            { 'B', 3},
+            { 'C', 3},
+            { 'M', 3},
+            { 'P', 3},
+            { 'F', 4},
+            { 'H', 4},
+            { 'V', 4},
+            { 'W', 4},
+            { 'Y', 4},
+            { 'K', 5},
+            { 'J', 8},
+            { 'X', 8},
+            { 'Q', 10},
+            { 'Z', 10 }
+        };
+
+        private string _word;
+        
         public Scrabble(string word)
-        {            
-            //TODO: do something with the word variable
+        {
+            _word = word;
         }
 
         public int score()
         {
-            //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int wordscore = 0;
+            bool doubleWord = false;
+
+            if (_word == " ")
+            {
+                return 0;
+            } 
+            else
+            {
+                string uppercase = _word.ToUpper();
+                char[] chars = uppercase.ToCharArray();
+                
+                foreach (var ch in chars)
+                {
+                    if (letterValue.ContainsKey(ch))
+                    {
+                        wordscore += letterValue[ch];
+                    }                   
+                }
+                //Doubleword true - ok this is a cheesy solve lmao xD
+                if (chars.Contains('}'))
+                {
+                    wordscore = wordscore * 2;
+                }
+
+                //Tripleword true
+                if (chars.Contains(']'))
+                {
+                    wordscore = wordscore * 3;
+                }
+                return wordscore;
+            }
         }
     }
 }
