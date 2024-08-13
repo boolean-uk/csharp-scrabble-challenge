89dd77ff123af80c4172c7ccf3a732f0021bfe3c
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-08-13 12:40:55 +0200

    Done all core tasks

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,58 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private Dictionary<char, int> _wordValuePairs = new Dictionary<char, int>();
+        private string _word;
+        
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+
+            _wordValuePairs.Add('a', 1);
+            _wordValuePairs.Add('e', 1);
+            _wordValuePairs.Add('i', 1);
+            _wordValuePairs.Add('o', 1);
+            _wordValuePairs.Add('u', 1);
+            _wordValuePairs.Add('l', 1);
+            _wordValuePairs.Add('n', 1);
+            _wordValuePairs.Add('r', 1);
+            _wordValuePairs.Add('s', 1);
+            _wordValuePairs.Add('t', 1);
+            _wordValuePairs.Add('d', 2);
+            _wordValuePairs.Add('g', 2);
+            _wordValuePairs.Add('b', 3);
+            _wordValuePairs.Add('c', 3);
+            _wordValuePairs.Add('m', 3);
+            _wordValuePairs.Add('p', 3);
+            _wordValuePairs.Add('f', 4);
+            _wordValuePairs.Add('h', 4);
+            _wordValuePairs.Add('v', 4);
+            _wordValuePairs.Add('w', 4);
+            _wordValuePairs.Add('y', 4);
+            _wordValuePairs.Add('k', 5);
+            _wordValuePairs.Add('j', 8);
+            _wordValuePairs.Add('x', 8);
+            _wordValuePairs.Add('q', 10);
+            _wordValuePairs.Add('z', 10);
+
+            _word = word.ToLower();
+
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            if (_word.Count() == 0) return 0;
+
+            int total = 0;
+            foreach (var item in _word.ToCharArray())
+            {
+                if (_wordValuePairs.ContainsKey(item))
+                {
+                    total += _wordValuePairs[item];
+                }
+            }
+            return total;
         }
     }
 }
