9ede4bf3f3a703702df9d15413a5cf8a3adbb233
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-08-13 15:18:15 +0200

    extension tests valid

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_scrabble_challenge.Main;
+
 Console.WriteLine("Hello, World!");
+Scrabble test0 = new Scrabble("");
+/* 
+
+Scrabble test2 = new Scrabble(" \t\n");
+Scrabble test3 = new Scrabble("a");
+Scrabble test4 = new Scrabble("f");
+Scrabble test5 = new Scrabble("street");
+Scrabble test6 = new Scrabble("quirky");
+Scrabble test7 = new Scrabble("OXyPHEnBUTaZoNE");
+
+Console.WriteLine(test1.score());
+Console.WriteLine(test2.score());
+Console.WriteLine(test3.score());
+Console.WriteLine(test4.score());
+Console.WriteLine(test5.score());
+Console.WriteLine(test6.score());
+Console.WriteLine(test7.score());
+*/
+Scrabble test = new Scrabble("dog");
+Scrabble test1 = new Scrabble("d{o}g");
+Scrabble test2 = new Scrabble("d[o]g");
+Scrabble test3 = new Scrabble("{dog}");
+
+Console.WriteLine(test0.score());
+Console.WriteLine(test1.score());
+Console.WriteLine(test2.score());
+Console.WriteLine(test3.score());
+
+
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,81 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        public char[] wrd;
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+            if (word == null) throw new ArgumentNullException("no word");
+            //turn string into list of chars
+            char[] chars = word.ToUpper().ToCharArray();
+            wrd = chars;
         }
 
         public int score()
         {
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+
+            int score = 0;
+            Dictionary<char[], int> point = new Dictionary<char[], int>();
+            point.Add(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 1);
+            point.Add(['D', 'G'], 2);
+            point.Add(['B', 'C', 'M', 'P'], 3);
+            point.Add(['F', 'H', 'V', 'W', 'Y'], 4);
+            point.Add(['K'], 5);
+            point.Add(['J', 'X'], 8);
+            point.Add(['Q', 'Z'], 10);
+
+
+            int pos = 0;
+
+            if(wrd.Length == 0)
+            {
+                return 0;
+            }
+
+            foreach (var character in wrd)
+            {
+                foreach (var row in point.Keys)
+                {
+                    foreach (var element in row)
+                    {
+                        if(character == element)
+                        {
+                            int nr = point[row];
+                            if (pos > 0)
+                            {
+                                if ((wrd[pos - 1] == '{') && (wrd[pos + 1] == '}'))
+                                {
+                                    score += (2 * nr);
+                                }
+                                if ((wrd[pos - 1] == '[') && (wrd[pos + 1] == ']'))
+                                {
+                                    score += (3 * nr);
+                                }
+                                else
+                                {
+                                    score += nr;
+                                }
+                            }
+                            else
+                            {
+                                score += nr;
+                            }
+                            
+                        }
+                    }
+                }
+                pos += 1;
+            }
+            if ((wrd.First() == '{') && (wrd.Last() == '}')){
+                score += score;
+            }
+            if ((wrd.First() == '[') && (wrd.Last() == ']')){
+                score += (2 *score);
+            }
+
+            return score;
+            //throw new NotImplementedException(); //TODO: Remove this line when the code has been written
         }
     }
 }
