d082f70efc121d2392dcada5b64fa1356acb5e0d
Enock97 <enockladu@hotmail.com>
2025-01-08 13:40:16 +0100

    Finished exercise

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -9,15 +10,69 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        string word;
+        int scoreCounter;
         public Scrabble(string word)
-        {            
+        {
             //TODO: do something with the word variable
+            this.word = word.ToUpper();
+            this.scoreCounter = 0;
         }
 
         public int score()
         {
+            foreach (char letter in word.ToCharArray())
+            {
+
+                switch (letter) // Ensure case insensitivity
+                {
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                    case 'L':
+                    case 'N':
+                    case 'R':
+                    case 'S':
+                    case 'T':
+                        scoreCounter += 1;
+                        break;
+                    case 'D':
+                    case 'G':
+                        scoreCounter += 2;
+                        break;
+                    case 'B':
+                    case 'C':
+                    case 'M':
+                    case 'P':
+                        scoreCounter += 3;
+                        break;
+                    case 'F':
+                    case 'H':
+                    case 'V':
+                    case 'W':
+                    case 'Y':
+                        scoreCounter += 4;
+                        break;
+                    case 'K':
+                        scoreCounter += 5;
+                        break;
+                    case 'J':
+                    case 'X':
+                        scoreCounter += 8;
+                        break;
+                    case 'Q':
+                    case 'Z':
+                        scoreCounter +=10;
+                        break;
+                    default:
+                        scoreCounter = 0; // Default value for invalid input
+                        break;
+                }
+            }
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            return scoreCounter;
         }
     }
 }
